73f034d3d78ddfde50d925ce919c763605b485f1
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-05-28 20:15:39 +0600

    Home Work 07.06.19

--- HomeWork07_06_19/MainWindow.xaml.cs
+++ HomeWork07_06_19/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,27 @@ namespace HomeWork07_06_19
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AnimationStartButtonClick(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = processRectangle.Width;
+            animation.To = processBarRectangle.Width - 30;
+            animation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            animation.Completed += new EventHandler(AnimationCompleted);
+
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(animation);
+            Storyboard.SetTargetName(animation, processRectangle.Name);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(WidthProperty));
+
+            storyboard.Begin(this);
+        }
+
+        private void AnimationCompleted(object sender, EventArgs e)
+        {
+            MessageBox.Show("Process finished");
         }
     }
 }
